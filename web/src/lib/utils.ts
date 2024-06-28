350d1e37ee672bba3e08010a2b19546e79f11af6
Jan Van den Enden <jan@enden.be>
2024-06-28 23:35:27 +0200

    add function to shorten addresses

--- web/src/lib/utils.ts
+++ web/src/lib/utils.ts
@@ -1,6 +1,11 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function shortenAddress(address: `0x${string}` | undefined): string | undefined {
+  if (!address) return address;
+  return `${address.slice(0, 5)}...${address.slice(-2)}`;
 }
