9705e2d7671340f798f1a1f6a63dc865e69c6f78
Ken Van den Enden <ken@enden.be>
2024-07-01 09:40:18 +0200

    Register profile.

--- web/src/hooks/useSearchUser.ts
+++ web/src/hooks/useSearchUser.ts
@@ -4,14 +4,14 @@ export default function useSearchUser(address?: `0x${string}`) {
   const query = useQuery({
     queryKey: ["user", address],
     queryFn: async () => {
-      if (!address) return;
+      if (!address) return null;
       return fetch(`/api/profile?${new URLSearchParams({ address })}`, {
         method: "GET",
         headers: {
           "Content-Type": "application/json",
         },
       }).then((res) => {
-        if (!res.ok) return;
+        if (!res.ok) return null;
 
         return res.json();
       });
